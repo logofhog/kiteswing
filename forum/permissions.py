0de891fe000126ef2f01d94c6982de44caa81035
logofhog <thomasscoggins@gmail.com>
2013-12-18 13:59:17 -0500

    edited permissions

--- forum/permissions.py
+++ forum/permissions.py
@@ -4,10 +4,8 @@ class ModPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user.customuser_related.get()
         if user in obj.moderators.all():
-            print 'trueu'
             return True
         else:
-            print 'flaslse'
             return False
 
 
